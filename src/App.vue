cb263b24dd9a2f80c1040f481e86766318140f76
embroid <wangxinyu.go@qq.com>
2022-12-19 21:56:56 +0800

    App.vue update

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,15 @@
 </template>
 
 <style lang="less">
-@import '@/assets/less/common.less';
 #app {
   position: relative;
   width: 100%;
-  height: auto;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: @background_color;
 }
 
 nav {
